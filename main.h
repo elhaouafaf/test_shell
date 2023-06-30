21a4c7c11e87f4d850bdf37ba333439700a54637
youssef AIT ELMAHI <125739100+youssef-ait-elmahi@users.noreply.github.com>
2023-06-30 23:23:39 +0000

    hello 2

--- main.h
+++ main.h
@@ -20,12 +20,13 @@ int _strcmp(const char *s1, char *s2);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 char *_strchr(char *s, char c);
-int _strlen(char *s);
+int _strlen(const char *s);
 int _putchar(char c);
 void handle_interrupt(int sig);
 char **handle_input(char **buf, size_t *buf_size);
 void free_args(char **args);
 void execute_command(char **args, char **cmd, char *buf);
 void handle_command(char **args);
+char *_strdup(const char *str);
 
 #endif
